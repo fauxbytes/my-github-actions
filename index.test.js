04b2ad14ac8589ba6c74d709e1f7be46d26a7cc9
fauxbytes <fauxbytes@not.me>
2022-11-09 12:28:01 +0200

    fail test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const { mooncakeSays } = require('./');
 
 test('mooncake knows what to say', () => {
-  expect(mooncakeSays('(o.o) woooahhh!')).toBe('(o.o) woooahhh!');
+  expect(mooncakeSays('foo')).toBe('(o.o) woooahhh!');
 });
 
 test('mooncake is not sure what to say', () => {
